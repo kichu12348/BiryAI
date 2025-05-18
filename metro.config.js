cc208ce1915bb60de33fe9dd212c533291d52405
Mahadevan Reji <rmahadevan574@gmail.com>
2025-05-18 16:23:00 +0530

    Remove 'bin' extension from asset resolver configuration

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,5 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('bin');
-
 
 module.exports = config;
